cbc77fd2c43882e44382010d5b3d02753d803cc7
Ashly Lau <ashlylau@gmail.com>
2020-05-09 19:17:33 +0800

    add some game logic

--- js/video.js
+++ js/video.js
@@ -1,73 +1,3 @@
-async function loadModel() {
-    const net = await bodyPix.load(/** optional arguments, see below **/);
-
-    console.log('here');
-}
-
-async function getSegmentedImage(img) {
-    const net = await bodyPix.load(/** optional arguments, see below **/);
-
-    const segmentation = await net.segmentPerson(img);
-    const array = segmentation.data;
-    console.log(array.includes(0));
-    console.log(array.includes(1));
-    console.log(segmentation.data);
-
-    return segmentation;
-}
-
-function drawImage(img, segmentation) {
-    // The mask image is an binary mask image with a 1 where there is a person and
-    // a 0 where there is not.
-    const coloredPartImage = bodyPix.toMask(segmentation);
-    const opacity = 0.7;
-    const flipHorizontal = false;
-    const maskBlurAmount = 0;
-    const canvas = document.getElementById('canvas');
-    // Draw the mask image on top of the original image onto a canvas.
-    // The colored part image will be drawn semi-transparent, with an opacity of
-    // 0.7, allowing for the original image to be visible under.
-    bodyPix.drawMask(
-        canvas, img, coloredPartImage, opacity, maskBlurAmount,
-        flipHorizontal);
-}
-
-/**
- * Draw an image on a canvas
- */
-function renderImageToCanvas(image, size, canvas) {
-    canvas.width = size[0];
-    canvas.height = size[1];
-    const ctx = canvas.getContext('2d');
-  
-    ctx.drawImage(image, 0, 0);
-}
-
-/**
- * Converts an array of pixel data into an ImageData object
- */
-async function renderToCanvas(a, ctx) {
-    // const [height, width] = a.shape;
-    const width = 400;
-    const height = 300;
-    const imageData = new ImageData(width, height);
-  
-    // const data = await a.data();
-    const data = a;
-  
-    for (let i = 0; i < height * width; ++i) {
-      const j = i * 4;
-      const k = i * 3;
-  
-      imageData.data[j + 0] = data[k + 0];
-      imageData.data[j + 1] = data[k + 1];
-      imageData.data[j + 2] = data[k + 2];
-      imageData.data[j + 3] = 255;
-    }
-  
-    ctx.putImageData(imageData, 0, 0);
-}
-
 /** Set up webcam */
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
@@ -90,37 +20,60 @@ navigator.getMedia({
     // error.code
 });
 
-loadModel();
+/** Start game */
+document.getElementById("start").addEventListener("click", function() {
+    displayCountdown();
+    setTimeout(() => {
+        takeScreenshot();
+    }, 4000);
+});
 
 /** Take screenshot */
 document.getElementById("snap").addEventListener("click", function() {
-    context.drawImage(video, 0, 0, 400, 300);
-
-    segmentImage();
+    takeScreenshot();
 });
+
+
+function takeScreenshot() {
+    context.drawImage(video, 0, 0, 800, 600);
+    segmentImage();
+}
 
 async function segmentImage() {
     const img = document.getElementById('canvas');
 
     const net = await bodyPix.load();
-    const segmentation = await net.segmentPerson(img);
+    const segmentation = await net.segmentMultiPersonParts(img);
 
-    // const segmentation = getSegmentedImage(img);
-
-    // renderToCanvas(array, context);
-    // drawImage(img, segmentation)
+    console.log(segmentation);
 
     // The mask image is an binary mask image with a 1 where there is a person and
     // a 0 where there is not.
-    const coloredPartImage = bodyPix.toMask(segmentation);
-    const opacity = 0.7;
+    const coloredPartImage = bodyPix.toColoredPartMask(segmentation);
+
+    console.log(img);
+    const opacity = 0.5;
     const flipHorizontal = false;
     const maskBlurAmount = 0;
-    const canvas = document.getElementById('canvas');
     // Draw the mask image on top of the original image onto a canvas.
     // The colored part image will be drawn semi-transparent, with an opacity of
     // 0.7, allowing for the original image to be visible under.
     bodyPix.drawMask(
         canvas, img, coloredPartImage, opacity, maskBlurAmount,
         flipHorizontal);
+}
+
+function displayCountdown() {
+    document.querySelector("#start").disabled = true;
+
+    var countDown = 3;
+
+    var t = setInterval(function() {
+        if (countDown <= 0) {
+          document.querySelector("#start").disabled = false;
+          clearInterval(t);
+        }
+        document.getElementById('timer').innerHTML = countDown;
+        countDown -= 1;
+      }, 1000);
 }
